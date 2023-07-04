66b72f9fb0db6b5755f0b447b6db418511c5e513
Abdullah Al Sayad <abdullahalsayad03@gmail.com>
2023-07-04 15:59:38 +0600

    modal created

--- admin/app/(root)/page.tsx
+++ admin/app/(root)/page.tsx
@@ -1,11 +1,25 @@
-import { UserButton } from "@clerk/nextjs";
+"use client";
+
+import { Modal } from "@/components/ui/modal";
+import { useStoreModal } from "@/hooks/use-store-modal";
+import { useEffect } from "react";
+// import { UserButton } from "@clerk/nextjs";
 
 const SetupPage = () => {
+  const onOpen = useStoreModal((state) => state.onOpen);
+  const isOpen = useStoreModal((state) => state.isOpen);
+
+  useEffect(() => {
+    if (!isOpen) {
+      onOpen();
+    }
+  }, [isOpen, onOpen]);
+
   return (
     <div>
-      <div>
-        <UserButton afterSignOutUrl="/" />
-      </div>
+      <Modal title="Test" descripton="Test" isOpen onClose={() => {}}>
+        Root Page
+      </Modal>
     </div>
   );
 };
